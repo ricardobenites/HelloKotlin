285a8f00871295bb993cdf139d61625eb9c6ae86
Ricardo <stigma746@gmail.com>
2023-02-14 17:31:36 -0300

    textoEditable del tts, cambio de idioma y mejoras de practicas

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -3,14 +3,13 @@ package com.example.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
-    var tts: TextToSpeech? = null
+    private var tts: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,17 +21,30 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     }
 
     private fun speak(){
-        var message: String = findViewById<TextView>(R.id.textView).text.toString()
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
+
+        if (message.isEmpty()){
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_text_empty)
+            message = "¿Es en serio? Ya pon algo en el edit Text!"
+        }
 
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS){
-            findViewById<TextView>(R.id.textView).text = "Listo!"
-            tts!!.setLanguage(Locale.US)
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_active)
+            tts!!.language = Locale("ES")
         } else {
-            findViewById<TextView>(R.id.textView).text = "No disponible :("
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_no_active)
         }
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
     }
 }
